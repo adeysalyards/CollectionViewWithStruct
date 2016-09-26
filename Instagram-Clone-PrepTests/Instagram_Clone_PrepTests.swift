d84516c05aef56eae5d32b54435f3a6b7267acb2
c1Codepath <adey.salyards@capitalone.com>
2016-09-26 18:03:38 -0400

    Initial Commit

---
+++ Instagram-Clone-PrepTests/Instagram_Clone_PrepTests.swift
@@ -0,0 +1,36 @@
+//
+//  Instagram_Clone_PrepTests.swift
+//  Instagram-Clone-PrepTests
+//
+//  Created by Salyards, Adey on 9/26/16.
+//  Copyright © 2016 Salyards, Adey. All rights reserved.
+//
+
+import XCTest
+@testable import Instagram_Clone_Prep
+
+class Instagram_Clone_PrepTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
